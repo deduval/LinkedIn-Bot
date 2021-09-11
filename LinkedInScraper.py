14983352db1e96fa65d1716ea006d1e3e739c12c
David Duval <de.duval6@gmail.com>
2021-09-10 23:16:14 -0700

    User can log into LinkedIn

--- LinkedInScraper.py
+++ LinkedInScraper.py
@@ -1,15 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
 import time
 
 load_dotenv()
 
+#get username and password from user
+username = input("Enter Email for LinkedIn: ")
+password = input("Password for LinkedIn: ")
+
+#goes to linkedin
 url = "https://www.linkedin.com/"
 driver = webdriver.Chrome(os.environ.get('CHROMEDRIVERPATH'))
 driver.get(url)
 
-print(driver.title)
-time.sleep(5)
+#fill username
+usernameField = driver.find_element_by_xpath('//*[@id="session_key"]')
+usernameField.send_keys(username)
 
-driver.quit()
+#fill password
+passwordField = driver.find_elements_by_xpath('//*[@id="session_password"]')
+passwordField[0].send_keys(password)
+
+#login
+loginButton = driver.find_element_by_class_name("sign-in-form__submit-button")
+loginButton.send_keys(Keys.RETURN)
